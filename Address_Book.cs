2c1752fa2118c829ddaeef32cae4d01ac3e81253
dikoleshubh <dikole.shubh@gmail.com>
2021-05-03 00:30:19 +0530

    Address Book - Remove Profile

--- Address_Book.cs
+++ Address_Book.cs
@@ -108,5 +108,30 @@ namespace Adress_Book
             }
         }
        
+        public static void remove()
+        {// Enter the name of person to remove
+            Console.WriteLine("Enter the first name of the person you would like to remove.");
+            string firstName = Console.ReadLine();
+            Person person = People.FirstOrDefault(x => x.FirstName.ToLower() == firstName.ToLower());
+            if (person == null) //Checking for it's existence
+            {
+                Console.WriteLine("That person could not be found. Press any key to continue");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Are you sure you want to remove this person from your address book? (Y/N)");
+                PrintPeople(person);//Confirmation
+
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                {
+                    People.Remove(person);
+                    Console.WriteLine("Person removed. Press any key to continue.");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
     
 }
